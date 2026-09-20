b99b7e686fa87b9667f93682d55b1de764d63406
KevinRaharjo <86224584+KevinRaharjo@users.noreply.github.com>
2026-09-21 02:08:03 +0700

    Create KRWBackend.h

---
+++ DualBackend/KRWBackend.h
@@ -0,0 +1,14 @@
+//
+//  KRWBackend.h
+//  Routing helpers; implementation in KRWBackend.m
+//
+
+#ifndef KRWBackend_h
+#define KRWBackend_h
+
+#import <Foundation/Foundation.h>
+#import <stdint.h>
+#import <stdbool.h>
+#import "BadKernelBridge.h"
+
+#endif /* KRWBackend_h */
