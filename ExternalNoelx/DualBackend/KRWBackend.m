5cf3dd69c6e948e55ce9db3559cda13e9b778fc3
KevinRaharjo <86224584+KevinRaharjo@users.noreply.github.com>
2026-09-21 03:05:35 +0700

    Update KRWBackend.m

--- ExternalNoelx/DualBackend/KRWBackend.m
+++ ExternalNoelx/DualBackend/KRWBackend.m
@@ -6,37 +6,30 @@
 #import "BadKernelBridge.h"
 #import "KRWBackend.h"
 
-// Forward-declare opa334 primitives so we don't have to import the whole header here.
 extern uint64_t early_kread64(uint64_t where);
 extern void early_kwrite64(uint64_t where, uint64_t what);
 
-static KRWBackendType gKRWBackend = KRWBackendTypeNone;
+static int32_t gKRWBackend = 0;
 
-void krw_set_backend(KRWBackendType type) {
+void krw_set_backend(int32_t type) {
     gKRWBackend = type;
     const char *name = "none";
-    switch (type) {
-        case KRWBackendTypeOpa334:    name = "opa334";    break;
-        case KRWBackendTypeBadKernel: name = "BadKernel"; break;
-        default:                       name = "none";     break;
-    }
+    if (type == 1) name = "opa334";
+    else if (type == 2) name = "BadKernel";
     NSLog(@"[KRW] backend = %s", name);
 }
 
-KRWBackendType krw_current_backend(void) { return gKRWBackend; }
+int32_t krw_current_backend(void) { return gKRWBackend; }
 
 uint64_t krw_backend_kread64(uint64_t addr) {
-    switch (gKRWBackend) {
-        case KRWBackendTypeBadKernel: return BadKernelKRead64(addr);
-        case KRWBackendTypeOpa334:
-        default:                       return early_kread64(addr);
-    }
+    if (gKRWBackend == 2) return BadKernelKRead64(addr);
+    return early_kread64(addr);
 }
 
 void krw_backend_kwrite64(uint64_t addr, uint64_t val) {
-    switch (gKRWBackend) {
-        case KRWBackendTypeBadKernel: BadKernelKWrite64(addr, val); return;
-        case KRWBackendTypeOpa334:
-        default:                       early_kwrite64(addr, val);  return;
+    if (gKRWBackend == 2) {
+        BadKernelKWrite64(addr, val);
+        return;
     }
+    early_kwrite64(addr, val);
 }
