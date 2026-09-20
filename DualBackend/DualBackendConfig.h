3d2adc0f1e7775c4e4197ceda6781bb8e56410c9
KevinRaharjo <86224584+KevinRaharjo@users.noreply.github.com>
2026-09-21 02:08:41 +0700

    Create DualBackendConfig.h

---
+++ DualBackend/DualBackendConfig.h
@@ -0,0 +1,15 @@
+//
+//  DualBackendConfig.h
+//  Compile-time feature flags.
+//
+
+#ifndef DualBackendConfig_h
+#define DualBackendConfig_h
+
+#define ENABLE_OPA334     1
+#define ENABLE_BADKERNEL  1
+#define ENABLE_BAD_QUERY  1
+#define ENABLE_MCM_BRIDGE 1
+#define ENABLE_OFFSETS    1
+
+#endif /* DualBackendConfig_h */
