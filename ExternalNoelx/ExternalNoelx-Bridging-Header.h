4544ff032c0c87fb73f8d23159a88b4bd2827c07
KevinRaharjo <86224584+KevinRaharjo@users.noreply.github.com>
2026-09-21 02:06:52 +0700

    Update ExternalNoelx-Bridging-Header.h

--- ExternalNoelx/ExternalNoelx-Bridging-Header.h
+++ ExternalNoelx/ExternalNoelx-Bridging-Header.h
@@ -1,3 +1,8 @@
+//
+//  ExternalNoelx-Bridging-Header.h
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
 #import "exploit/bad_query.h"
 #import "exploit/mcm_bridge.h"
 #import "kexploit/kexploit_opa334.h"
@@ -5,3 +10,11 @@
 #import "kexploit/kutils.h"
 #import "helpers/AppIconHelper.h"
 #import "helpers/DisplayIdentity.h"
+
+// Dual-backend (iOS 26-27)
+#import "DualBackend/BadKernelBridge.h"
+#import "DualBackend/KRWBackend.h"
+#import "DualBackend/DualBackendConfig.h"
+
+// BadKernel (cloned at build time into ExternalNoelx/BadKernel/)
+#import "BadKernel/BadKernel.h"
